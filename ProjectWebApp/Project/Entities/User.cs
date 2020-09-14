64e58f3d1394ed8e0e8c1cd4f517fedf6dd4776a
Thomas <thomasreuvers@upcmail.nl>
2020-09-14 23:25:24 +0200

    Add User entity

---
+++ ProjectWebApp/Project/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Project.Entities
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string EmailAddress { get; set; }
+
+        public string SecretUserKey { get; set; }
+
+        public List<string> Roles = new List<string>();
+    }
+}
