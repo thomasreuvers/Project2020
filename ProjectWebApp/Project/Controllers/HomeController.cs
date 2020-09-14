a9366983946c04c71e13037ae358ee21f5aec180
Thomas <thomasreuvers@upcmail.nl>
2020-09-14 23:25:49 +0200

    Update HomeController for testing.

--- ProjectWebApp/Project/Controllers/HomeController.cs
+++ ProjectWebApp/Project/Controllers/HomeController.cs
@@ -3,25 +3,37 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Project.Entities;
 using Project.Models;
+using Project.Services;
 
 namespace Project.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserService _userService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserService userService)
         {
             _logger = logger;
+            _userService = userService;
         }
 
         public IActionResult Index()
         {
+
             return View();
         }
+
+        #region Testing DB connection and custom authentication
+
+        
+
+        #endregion
 
         public IActionResult Privacy()
         {
