527c742143311b580dbc5fe893adeb24cabe81b6
Thomas <thomasreuvers@upcmail.nl>
2020-09-19 00:51:03 +0200

    Update User.cs

--- ProjectWebApp/Project/Entities/User.cs
+++ ProjectWebApp/Project/Entities/User.cs
@@ -14,6 +14,7 @@ namespace Project.Entities
         public string Id { get; set; }
         public string Username { get; set; }
         public string PasswordHash { get; set; }
+        public string Salt { get; set; }
         public string EmailAddress { get; set; }
 
         public string SecretUserKey { get; set; }
