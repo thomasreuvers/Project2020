3eef09dd00421cc853da79c37c023d219fa9057c
Thomas <thomasreuvers@upcmail.nl>
2020-09-21 23:18:16 +0200

    Update LoginModel.cs

--- ProjectWebApp/Project/Models/LoginModel.cs
+++ ProjectWebApp/Project/Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,12 @@ namespace Project.Models
 {
     public class LoginModel
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
