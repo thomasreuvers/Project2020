0a027536c05b8afa815398f0d11853d825bd41d0
Thomas <thomasreuvers@upcmail.nl>
2020-09-21 15:58:41 +0200

    Add Extensions method class

---
+++ ProjectWebApp/Project/ExtensionMethods/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.ExtensionMethods
+{
+    public static class Extensions
+    {
+        public static string RandomString(this string str)
+        {
+            var rnd = new Random();
+
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 8)
+                .Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+    }
+}
