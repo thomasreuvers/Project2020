04e64c779da93f7e67595d23d1a6df07bda70128
Thomas <thomasreuvers@upcmail.nl>
2020-09-21 23:18:03 +0200

    Remove redundant login code from HomeController

--- ProjectWebApp/Project/Controllers/HomeController.cs
+++ ProjectWebApp/Project/Controllers/HomeController.cs
@@ -33,62 +33,6 @@ namespace Project.Controllers
             return View();
         }
 
-        #region Testing DB Connection and custom authentication
-
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginModel model)
-        {
-            if (model == null)
-            {
-                ModelState.AddModelError("emptyModel", "Model is empty");
-                return RedirectToAction("Index", "Home");
-            }
-
-            // Get user by email if not null
-            var user = await _userService.GetUserByEmailTask(model.EmailAddress);
-            if (user == null)
-            {
-                ModelState.AddModelError("userIsNull", "The user doesn't exist or could not be retrieved from the Db.");
-                return RedirectToAction("Index", "Home");
-            }
-
-            // Validate entered password against stored hashed and salted password
-            var cryptographyProcessor = new CryptographyProcessor();
-            var isValidUser = cryptographyProcessor.VerifyHashedPassword(model.Password, user.PasswordHash, user.Salt);
-            if (!isValidUser)
-            {
-                return RedirectToAction("Index", "Home");
-            }
-
-
-            /* Create the identity
-             * Add other user info later on
-             */
-            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
-            identity.AddClaim(new Claim(ClaimTypes.Name, user.Username));
-            identity.AddClaim(new Claim(ClaimTypes.Email, user.EmailAddress));
-
-            // Add roles
-            foreach (var role in user.Roles)
-            {
-                identity.AddClaim(new Claim(ClaimTypes.Role, role));
-            }
-
-            // Sign in
-            var principle = new ClaimsPrincipal(identity);
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principle);
-
-            return RedirectToAction("Index", "Home");
-        }
-
-        public async Task<IActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync();
-            return RedirectToAction("Index", "Home");
-        }
-
-        #endregion
-
         public IActionResult Privacy()
         {
             return View();
