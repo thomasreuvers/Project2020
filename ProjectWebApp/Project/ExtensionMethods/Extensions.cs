8221bf49150cabb3cbbffcb6e076f2520ac3b4b6
Thomas <thomasreuvers@upcmail.nl>
2020-11-06 15:33:02 +0100

    Add selected function that sets the current selected menu item as 'selected' class

--- ProjectWebApp/Project/ExtensionMethods/Extensions.cs
+++ ProjectWebApp/Project/ExtensionMethods/Extensions.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Project.ExtensionMethods
 {
     public static class Extensions
     {
+        public static string IsSelected(this IHtmlHelper htmlHelper, string controllers, string actions, string cssClass = "selected")
+        {
+            string currentAction = htmlHelper.ViewContext.RouteData.Values["action"] as string;
+            string currentController = htmlHelper.ViewContext.RouteData.Values["controller"] as string;
+
+            IEnumerable<string> acceptedActions = (actions ?? currentAction).Split(',');
+            IEnumerable<string> acceptedControllers = (controllers ?? currentController).Split(',');
+
+            return acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController) ?
+                cssClass : String.Empty;
+        }
+
         public static string RandomString(this string str)
         {
             var rnd = new Random();
